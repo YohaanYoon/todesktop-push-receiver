b0af4616a7def0f29c5d316be9bfe8b97541c5af
Yohaan Yoon <straightfor@classting.com>
2021-07-28 13:35:53 +0900

    feat: add OneSignal
    - add OneSignal
    
    refs

--- OneSignal.js
+++ OneSignal.js
@@ -1,133 +1,147 @@
-const ONESIGNAL_VERSION = 10100;
-const ONESIGNAL_HOST_URL = 'https://onesignal.com/api/v1/';
-const ONESIGNAL_LOGGING = false;
-const ONESIGNAL_GAME_VERSION = '101.0.0';
+(function () {
+  'use strict';
 
-var __OneSignalHelper = {
-  sendToOneSignalApi: function(url, action, inData, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.onerror = function() {
-      OneSignal.log('Error connecting to OneSignal servers:' + url)
-    };
-    xhr.onload = function() {
-      if (callback) {
-        callback(xhr.response);
-      }
-    };
+  const ONESIGNAL_VERSION = 10100;
+  const ONESIGNAL_HOST_URL = 'https://onesignal.com/api/v1/';
+  const ONESIGNAL_LOGGING = false;
+  const ONESIGNAL_GAME_VERSION = '101.0.0';
 
-    xhr.open(action, ONESIGNAL_HOST_URL + url);
-    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    xhr.send(JSON.stringify(inData));
-  }
-};
-
-var OneSignal = {
-  log: function(message) {
-    if (ONESIGNAL_LOGGING) {
-      console.log(message);
-    }
-  },
-
-  registerWithOneSignal: function(appId, registrationId, userId, tagsToSendOnRegister) {
-    var playerId = window.localStorage.getItem('player_id');
-    var requestUrl = 'players';
-
-    if (playerId !== null) {
-      requestUrl = 'players/' + playerId + '/on_session';
-    }
-
-    var jsonData = {
-      app_id: appId,
-      device_type: 4,
-      identifier: registrationId,
-      language: window.navigator.language ? window.navigator.language.substring(0, 2) : 'en',
-      timezone: (new Date().getTimezoneOffset() * -60),
-      device_model: 'ClasstingDesktopApp',
-      device_os: navigator.appVersion,
-      game_version: ONESIGNAL_GAME_VERSION,
-      sdk: ONESIGNAL_VERSION
-    };
-
-    if (userId !== null) {
-      jsonData['external_user_id'] = userId;
-    }
-
-    __OneSignalHelper.sendToOneSignalApi(requestUrl, 'POST', jsonData,
-      function registeredCallback(response) {
-        var responseJSON = JSON.parse(response);
-        window.localStorage.setItem('app_id', appId);
-        window.localStorage.setItem('player_id', responseJSON.id);
-
-        if (tagsToSendOnRegister !== null) {
-          OneSignal.sendTags(tagsToSendOnRegister);
+  var __OneSignalHelper = {
+    sendToOneSignalApi: function(url, action, inData, callback) {
+      const xhr = new XMLHttpRequest();
+      xhr.onerror = function() {
+        OneSignal.log('Error connecting to OneSignal servers:' + url)
+      };
+      xhr.onload = function() {
+        if (callback) {
+          callback(xhr.response);
         }
-      }
-    );
-  },
+      };
 
-  sendTag: function (key, value) {
-    var jsonKeyValue = {};
-    jsonKeyValue[key] = value;
-    OneSignal.sendTags(jsonKeyValue);
-  },
-
-  sendTags: function (jsonPair) {
-    var appId = window.localStorage.getItem('app_id');
-    var playerId = window.localStorage.getItem('player_id');
-
-    if (playerId !== null) {
-      __OneSignalHelper.sendToOneSignalApi('players/' + playerId, 'PUT', {app_id: appId, tags: jsonPair});
+      xhr.open(action, ONESIGNAL_HOST_URL + url);
+      xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+      xhr.send(JSON.stringify(inData));
     }
-  },
+  };
 
-  deleteTag: function (key) {
-    OneSignal.deleteTags([key]);
-  },
+  var OneSignal = {
+    log: function(message) {
+      if (ONESIGNAL_LOGGING) {
+        console.log(message);
+      }
+    },
 
-  deleteTags: function (keyArray) {
-    var jsonPair = {};
-    var length = keyArray.length;
-    for (var i = 0; i < length; i++)
-      jsonPair[keyArray[i]] = '';
+    registerWithOneSignal: function(appId, registrationId, userId, tagsToSendOnRegister) {
+      var playerId = window.localStorage.getItem('player_id');
+      var requestUrl = 'players';
 
-    OneSignal.sendTags(jsonPair);
-  },
+      if (playerId !== null) {
+        requestUrl = 'players/' + playerId + '/on_session';
+      }
 
-  getTags: function (callback) {
-    var playerId = window.localStorage.getItem('player_id');
+      var jsonData = {
+        app_id: appId,
+        device_type: 4,
+        identifier: registrationId,
+        language: window.navigator.language ? window.navigator.language.substring(0, 2) : 'en',
+        timezone: (new Date().getTimezoneOffset() * -60),
+        device_model: 'ClasstingDesktopApp',
+        device_os: navigator.appVersion,
+        game_version: ONESIGNAL_GAME_VERSION,
+        sdk: ONESIGNAL_VERSION
+      };
 
-    if (playerId !== null) {
-      __OneSignalHelper.sendToOneSignalApi('players/' + playerId, 'GET', {},
+      if (userId !== null) {
+        jsonData['external_user_id'] = userId;
+      }
+
+      __OneSignalHelper.sendToOneSignalApi(requestUrl, 'POST', jsonData,
         function registeredCallback(response) {
-          callback(JSON.parse(response).tags);
+          var responseJSON = JSON.parse(response);
+          window.localStorage.setItem('app_id', appId);
+          window.localStorage.setItem('player_id', responseJSON.id);
+
+          if (tagsToSendOnRegister !== null) {
+            OneSignal.sendTags(tagsToSendOnRegister);
+          }
         }
       );
-    }
-  },
+    },
 
-  addListenerForNotificationOpened: function (notifiationId) {
-    var appId = window.localStorage.getItem('app_id');
-    var playerId = window.localStorage.getItem('player_id');
+    sendTag: function (key, value) {
+      var jsonKeyValue = {};
+      jsonKeyValue[key] = value;
+      OneSignal.sendTags(jsonKeyValue);
+    },
 
-    __OneSignalHelper.sendToOneSignalApi('notifications/' + notifiationId, 'PUT',
-      {app_id: appId, player_id: playerId, opened: true}
-    );
-  },
+    sendTags: function (jsonPair) {
+      var appId = window.localStorage.getItem('app_id');
+      var playerId = window.localStorage.getItem('player_id');
 
-  addListenerForReceivedNotification: function (title, notification, callback) {
-    var myNotification = new Notification(title, {
-      body: notification.data.alert
-    });
+      if (playerId !== null) {
+        __OneSignalHelper.sendToOneSignalApi('players/' + playerId, 'PUT', {app_id: appId, tags: jsonPair});
+      }
+    },
 
-    var customJSON = JSON.parse(notification.data.custom);
-    var notifiationId = customJSON.i;
+    deleteTag: function (key) {
+      OneSignal.deleteTags([key]);
+    },
 
-    myNotification.onclick = function () {
-      OneSignal.addListenerForNotificationOpened(notifiationId);
-      if (callback) {
-        callback(customJSON.a);
+    deleteTags: function (keyArray) {
+      var jsonPair = {};
+      var length = keyArray.length;
+      for (var i = 0; i < length; i++)
+        jsonPair[keyArray[i]] = '';
+
+      OneSignal.sendTags(jsonPair);
+    },
+
+    getTags: function (callback) {
+      var playerId = window.localStorage.getItem('player_id');
+
+      if (playerId !== null) {
+        __OneSignalHelper.sendToOneSignalApi('players/' + playerId, 'GET', {},
+          function registeredCallback(response) {
+            callback(JSON.parse(response).tags);
+          }
+        );
+      }
+    },
+
+    addListenerForNotificationOpened: function (notifiationId) {
+      var appId = window.localStorage.getItem('app_id');
+      var playerId = window.localStorage.getItem('player_id');
+
+      __OneSignalHelper.sendToOneSignalApi('notifications/' + notifiationId, 'PUT',
+        {app_id: appId, player_id: playerId, opened: true}
+      );
+    },
+
+    addListenerForReceivedNotification: function (title, notification, callback) {
+      var myNotification = new Notification(title, {
+        body: notification.data.alert
+      });
+
+      var customJSON = JSON.parse(notification.data.custom);
+      var notifiationId = customJSON.i;
+
+      myNotification.onclick = function () {
+        OneSignal.addListenerForNotificationOpened(notifiationId);
+        if (callback) {
+          callback(customJSON.a);
+        }
       }
     }
-  }
-};
+  };
 
+  if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(function() {
+      return OneSignal;
+    });
+  } else if (typeof module !== 'undefined' && module.exports) {
+    module.exports.OneSignal = OneSignal;
+  } else {
+    window.OneSignal = OneSignal;
+  }
+}());
